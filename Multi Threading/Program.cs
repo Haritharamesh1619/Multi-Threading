5de060698e4a1020ce5336e0f7e7e99b11b348f7
Haritha R <harithalaks1998@gmail.com>
2024-07-15 19:18:34 +0530

    Add project files.

---
+++ Multi Threading/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threading
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Create and start the first thread
+            Thread thread1 = new Thread(PrintNumbers);
+            thread1.Name = "Thread 1";
+            thread1.Start();
+
+            // Create and start the second thread
+            Thread thread2 = new Thread(PrintNumbers);
+            thread2.Name = "Thread 2";
+            thread2.Start();
+
+            // Wait for both threads to complete
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Both threads have completed.");
+            Console.ReadKey();
+        }
+
+        static void PrintNumbers()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {i}");
+                Thread.Sleep(500); // Sleep for 500 milliseconds
+            }
+        }
+    }
+}
